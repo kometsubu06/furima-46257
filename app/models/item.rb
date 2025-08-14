87edf7b81785cc5b7f01b73481ef8889259584ec
kometsubu06 <ayaka.tai06@gmail.com>
2025-08-14 12:13:14 +0900

    itemモデルとの関連付けとバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
   with_options presence: true do
     validates :image
     validates :item_name
     validates :description
     validates :price
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_day_id
   end
 end
