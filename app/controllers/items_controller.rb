15c2e1aad24e20607ee989207b8bdc44c1477994
kometsubu06 <ayaka.tai06@gmail.com>
2025-08-14 12:29:37 +0900

    itemsコントローラーへ記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).params(:image, :item_name, :description, :category_id, :item_status_id, :shipping_cost_id,
+                                 :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
 end
