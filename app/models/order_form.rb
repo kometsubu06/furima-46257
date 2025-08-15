7a5321f70550c795e8622744497bb2c993d9e8fe
kometsubu06 <ayaka.tai06@gmail.com>
2025-08-16 00:51:02 +0900

    Formオブジェクトの作成

---
+++ app/models/order_form.rb
@@ -0,0 +1,20 @@
+class OrderForm
+  include ActiveModel::Model
+  attr_accessor :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :item_id, :user_id, :token
+
+  with_options presence: true do
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: "is invalid" }
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
+end
