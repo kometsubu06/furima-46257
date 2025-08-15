e313527e449425c9a948c0814617ddb61cf56980
kometsubu06 <ayaka.tai06@gmail.com>
2025-08-16 00:53:19 +0900

    コントローラーの作成と記述

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,35 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
+
+  def index
+    @order_form = OrderForm.new
+  end
+
+  def create
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_form).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if @item.user_id == current_user.id || @item.order.present?
+      redirect_to root_path
+    end
+  end
+end
