ee662f78f3b7b08753f13cb2c59771de3f943c42
kometsubu06 <ayaka.tai06@gmail.com>
2025-08-16 01:35:12 +0900

    アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,8 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
   belongs_to :user
   has_one_attached :image
-
+  has_one :order
+  
   with_options presence: true do
     validates :image
     validates :item_name
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :items do
+    resources :orders, only: [:index, :create]
+  end
 end
