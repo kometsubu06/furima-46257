7b2f2d440c75a47c39083290ed539613268cde5e
kometsubu06 <ayaka.tai06@gmail.com>
2025-08-17 12:33:49 +0900

    Addressモデルとテーブルの作成

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -15,6 +15,6 @@ class OrderForm
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    ShippingAddress.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
