df2ac9c03e4608ed33305cd5d787a7c46a8f407d
kometsubu06 <ayaka.tai06@gmail.com>
2025-08-19 02:34:18 +0900

    orderモデルテスト実装

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :order do
-    item { nil }
-    user { nil }
-    token {"tok_abcdefghijk00000000000000000"}
+    association :user
+    association :item
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,27 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  before do
-    @order = FactoryBot.build(:order)
-  end
-
-  context '内容に問題ない場合' do
-    it "priceとtokenがあれば保存ができること" do
-      expect(@order).to be_valid
-    end
-  end
-
-  context '内容に問題がある場合' do
-    it "priceが空では保存ができないこと" do
-      @order.price = nil
-      @order.valid?
-      expect(@order.errors.full_messages).to include("Price can't be blank")
+  describe '購入記録の保存' do
+    before do
+      # Orderをテストするには、関連するUserとItemが必要です。
+      user = create(:user)
+      item = create(:item)
+      # 作成したuserとitemに関連付くorderのインスタンスを生成します。
+      @order = build(:order, user: user, item: item)
     end
 
-    it "tokenが空では登録できないこと" do
-      @order.token = nil
-      @order.valid?
-      expect(@order.errors.full_messages).to include("Token can't be blank")
+    context '内容に問題ない場合' do
+      it 'userとitemが紐付いていれば保存できる' do
+        expect(@order).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'userが紐付いていないと保存できない' do
+        @order.user = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include('User must exist')
+      end
+      it 'itemが紐付いていないと保存できない' do
+        @order.item = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include('Item must exist')
+      end
     end
   end
 end
